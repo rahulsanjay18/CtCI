eb99c42888f6f10f581eca714f3baab8102a239c
Rahul Shah <rahulsanjay18@gmail.com>
2020-06-28 00:44:06 -0400

    added stack classes and comments

--- Makefile.mak
+++ Makefile.mak
@@ -1,7 +1,7 @@
 ﻿TARGET=dsa.out
 INCLUDES = $(wildcard *.h)
-DEPS = linked_list.h linked_list_test.h
-SOURCES = linked_list.c linked_list_test.c main.c 
+DEPS = linked_list.h linked_list_test.h stack_ll.h stack_ll_test.h stack_arr.h stack_arr_test.h
+SOURCES = linked_list.c linked_list_test.c stack_ll.c stack_ll_test.c stack_arr.h stack_arr_test.h main.c 
 OBJECTS=$(patsubst %.c, %.o, $(SOURCES))
 CFLAGS  = -c -g -Wall
 
---
+++ stack_ll_test.c
@@ -0,0 +1,11 @@
+﻿// Data struct: has a node object that is functionally the same as the linked list, and also a count. 
+// The top of the stack is the head of the linked list
+// NOTHING FROM ll methods can be used. 
+
+// Methods:
+
+// Push: take an element (int) and add that to the top of the stack. Maybe have one that also takes in a node object. No ll methods can be used (because that would be cheating)
+
+// Pop: remove the first element, and return the int value (maybe have one that returns a node too)
+
+// Peek: return first value/node
